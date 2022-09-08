83cd992701b30ea2ed11a7d5ddc0ef05c8245bad
berkay <berkayert7@gmail.com>
2022-09-08 16:43:07 +0300

    Initial Commit

---
+++ Shared/day21App.swift
@@ -0,0 +1,17 @@
+//
+//  day21App.swift
+//  Shared
+//
+//  Created by Berkay Ertürk on 8.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct day21App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
